20445de6324deafee67adee76cc1ee687870fb23
Carlos Guzmán <carlos.rak@gmail.com>
2024-11-26 13:23:02 +0100

    resolve response time

--- apps/server/src/schema/resolvers/queries/availability.ts
+++ apps/server/src/schema/resolvers/queries/availability.ts
@@ -1,22 +1,22 @@
 // resolvers/queries/availability.ts
-// Resolver for the "availability" query
 
 import { availability } from "../../../services/netstorming/queries/queries";
+import { generateUniqueId } from "../../../utils/helpers";
 import { AvailabilityParamsInput, AvailabilityResponse } from "../../types";
 import { createHotelConnection } from "../hotel/hotelConnection";
 
 export const availabilityResolver = async (
   parent: any,
-  args: { params: AvailabilityParamsInput }, // Define the expected input type
+  args: { params: AvailabilityParamsInput },
   context: any
 ): Promise<AvailabilityResponse> => {
-  console.log("[Resolvers] In availability query resolver, arguments: ", args);
+  const startTime = Date.now();
 
-  // Step 1: Fetch hotels from Netstorming
+  // Fetch Netstorming availability
   const hotels = await availability(args.params);
-  console.log("[Resolvers] Fetched hotels from Netstorming:", hotels.length);
+  const prvEndTime = Date.now();
 
-  // Step 2: Use createHotelConnection to generate a connection with extracted parameters
+  // Create new connection with extracted parameters
   const hotelsConnection = createHotelConnection(
     hotels,
     args.params?.sort ?? undefined,
@@ -24,11 +24,18 @@ export const availabilityResolver = async (
     args.params?.after ?? undefined
   );
 
-  // Step 3: Return AvailabilityResponse with the hotel connection
+  const endTime = Date.now();
+  const prvResponseTime = ((prvEndTime - startTime) / 1000).toFixed(3);
+  const totalResponseTime = ((endTime - startTime) / 1000).toFixed(3);
+
+  // Availability response
   return {
-    id: "availability-id", // Generate a unique ID
-    params: args.params, // Pass search parameters
-    responseTime: { total: 0, provider: 0 }, // Mock timing info
-    hotelsConnection: hotelsConnection, // Pass the hotel connection
+    id: generateUniqueId(),
+    params: args.params,
+    responseTime: {
+      total: parseFloat(totalResponseTime),
+      provider: parseFloat(prvResponseTime),
+    },
+    hotelsConnection: hotelsConnection,
   };
 };
